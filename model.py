36b543d0f35064fa72fc37f005826bf4d0971dc7
shuaichen zhou <shuaichen1220@gmail.com>
2017-11-25 17:14:57 -0500

    with some comment

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
+#load images from csv file
 with open('/data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
@@ -10,14 +11,17 @@ with open('/data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
+#augment and preprocess images
 for line in lines[1:]:
 	for i in range(3):
 		source_path = line[i].strip()
 		current_path = '/data/'+source_path
 		image = cv2.imread(current_path)
 		images.append(image)
+		#flip data
 		images.append(cv2.flip(image,1))
 		measurement = float(line[3])
+		#use three cameras
 		if i == 0 :
 			measurements.append(measurement)
 			measurements.append(measurement*-(1.0))
@@ -38,7 +42,7 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 import tensorflow as tf
 
-
+# Model from Nvidia paper
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
@@ -55,7 +59,7 @@ model.add(Dense(1))
 
 
 
-
+#Compile and fit the model
 model.compile(loss='mse',optimizer='adam')
 model.fit(X_train,y_train,validation_split=0.2,batch_size=128,shuffle=True,nb_epoch=5)
 model.save('/output/model.h5')
